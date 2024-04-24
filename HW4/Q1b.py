b81cbd2d1a314ca9db9fdb1869e4830e5d4ca391
Krista <krph6495@colorado.edu>
2024-04-24 02:58:17 +0000

    small update

--- HW4/Q1b.py
+++ HW4/Q1b.py
@@ -10,9 +10,10 @@ def main():
     data = Q1a.I_from_incidence('data/all_weeks.csv')
     last_points = data.iloc[-100:]
     i_eq = last_points["I(t)"].mean()
+    err = last_points["I(t)"].std()
     m,b = Q1a.plot_lin_reg(last_points,'Week','I(t)','figs/Q1_EQMPeriod',linReg=True)
     print(f'Slope = {m}, Intercept = {b}')
-    print(f'Average = {i_eq}')
+    print(f'Average = {i_eq}, Std Dev = {err}')
 
 
 if __name__ == '__main__':
